5f4f1514a4c29d57f187b92a5860a89827bf4306
max liu <max.liu@digitimes.com>
2024-12-30 16:13:42 +0800

    feat: Add edit functionality to TodoList (main)
    Added edit functionality to the TodoList component.
    Users can now edit todo items directly in the list.
    A debounce mechanism is implemented to prevent
    excessive API calls during editing.  Uses `FiEdit2`
    icon for edit action.

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import useTodos from "./hooks/useTodos";
 import { MdDeleteForever } from "react-icons/md";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import todosService, { Todo } from "./services/todosService";
 import { CACHE_KEY_TODOS } from "./const";
+import { FiEdit2 } from "react-icons/fi";
 
 
 const TodoList = () => {
@@ -30,9 +31,10 @@ interface Item {
 }
 
 function Item({ todo }: Item) {
+	const [ isEditing, setIsEditing ] = React.useState(false);
 	const queryClient = useQueryClient();
 	const mutation = useMutation<Todo, Error, Todo>({
-		mutationFn: async (todo) => todosService.delete(todo.id),
+		mutationFn: (todo) => todosService.delete(todo.id),
 		onSuccess: () => {
 			// Invalidate queries to refresh data after deletion
 			queryClient.invalidateQueries({ queryKey: CACHE_KEY_TODOS }).then(() => {
@@ -43,17 +45,60 @@ function Item({ todo }: Item) {
 			console.error("Deletion failed:", error);
 		},
 	});
-
+	const handleEdit = () => {
+		setIsEditing(prevState => !prevState);
+	};
 	const handleDelete = () => {
 		mutation.mutate(todo);
 	};
 	return (
 		<li key={todo.id} className="list-group-item">
 			<div className="d-flex justify-content-between align-content-center">
-				<span>{todo.title}</span>
-				<span style={{cursor:'pointer'}} onClick={() => handleDelete()}><MdDeleteForever/></span>
+				{
+					isEditing ? <EditableInput data={todo}/> : <span>{todo.title}</span>
+				}
+				<div style={{ width: '60px', display: 'flex', justifyContent: 'space-between', paddingLeft: '12px' }}>
+					<span style={{ cursor: 'pointer' }} onClick={() => handleEdit()}><FiEdit2/></span>
+					<span style={{ cursor: 'pointer' }} onClick={() => handleDelete()}><MdDeleteForever/></span>
+				</div>
 			</div>
 		</li>
 	);
 
+}
+
+function EditableInput({ data }: { data: Todo }) {
+	const [ value, setValue ] = useState(data.title);
+	const [timer, setTimer] = useState(0);
+	const queryClient = useQueryClient();
+	const mutation = useMutation<Todo, Error, Todo>({
+		mutationFn: (data) => todosService.put(data.id, data),
+		onSuccess: () => {
+			// Invalidate queries to refresh data after deletion
+			queryClient.invalidateQueries({ queryKey: CACHE_KEY_TODOS }).then(() => {
+				console.log('updated successfully')
+			});
+		},
+		onError: (error) => {
+			console.error("updated failed:", error);
+		},
+	});
+	const handleUpdate = (e: React.ChangeEvent<HTMLInputElement>) => {
+		// Update the input value
+		setValue(e.target.value);
+		// Debounce the input to avoid multiple requests
+		clearTimeout(timer);
+		setTimer(setTimeout(() => {
+			mutation.mutate({ ...data, title: e.target.value });
+		}, 1000));
+	};
+	useEffect(() => {
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [timer]);
+
+	return (
+		<input className='form-control' type="text" value={value} onChange={handleUpdate}/>
+	);
 }
